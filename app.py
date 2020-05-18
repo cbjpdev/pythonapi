33a26794f410e1d4d5617d98cfc52400bdd38c09
Chathuranga Pathirana <cbjpathirana@gmail.com>
2020-05-19 00:52:31 +0800

    ssecond commit

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return "Welcome!"
 
-@app.route('/')
+@app.route('/how are you')
 def hello():
-    return "Hello World!"
+    return 'I am good, how about you?'
 
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
